c68c984150ac622e939eb9605e7262bad9b78fda
Ashwath Shetty <ashwath98@pop-os.localdomain>
2022-03-02 23:15:48 +0100

    colab compatible scripts

--- dataloader.py
+++ dataloader.py
@@ -44,9 +44,42 @@ class RandomMaskDataset(Dataset):
 
         return X, y, mask
 
+
+class RandomMaskDataset(Dataset):
+    """Face Landmarks dataset."""
+
+    def __init__(self, dataset, mask_pixels):
+        """
+        Args:
+            csv_file (string): Path to the csv file with annotations.
+            root_dir (string): Directory with all the images.
+            transform (callable, optional): Optional transform to be applied
+                on a sample.
+        """
+        self.base_dataset = dataset
+        self.mask_pixels  = mask_pixels
+
+    def __len__(self):
+        return len(self.base_dataset)
+
+    def __getitem__(self, idx):
+        
+        X, y = self.base_dataset[idx]
+
+        mask          = torch.zeros(X.shape[1] * X.shape[2])
+        choice_list   = [i for i in range(len(mask))]
+        choices       = random.choices(choice_list, k = self.mask_pixels)
+
+        mask[choices] = 1
+        mask          = mask.view(X.shape[1], X.shape[2])
+        
+
+        return X, y, mask
+
 class CustomCIFAR10DataModule(LightningDataModule):
-    def __init__(self, batch_size=64):
+    def __init__(self, mask_pixels, batch_size=64):
         super().__init__()
+        self.mask_pixels=mask_pixels
         self.batch_size = batch_size
         self.transform_train =  transforms.Compose([
                                 transforms.RandomCrop(32, padding=4),
@@ -64,11 +97,20 @@ class CustomCIFAR10DataModule(LightningDataModule):
         CIFAR10(os.getcwd(), train=True,  download=True, transform=self.transform_train)
         CIFAR10(os.getcwd(), train=False, download=True, transform=self.transform_test)
 
+        self.transform_test   = transforms.Compose([
+                                  transforms.ToTensor(),
+                                  transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+                                  ])
+
+    def prepare_data(self):
+        CIFAR10(os.getcwd(), train=True,  download=True, transform=self.transform_train)
+        CIFAR10(os.getcwd(), train=False, download=True, transform=self.transform_test)
+
     def setup(self):
         transform      = transforms.Compose([transforms.ToTensor()])
         cifar10_train  = CIFAR10(os.getcwd(), train=True,  download=False, transform=self.transform_train)
         cifar10_test   = CIFAR10(os.getcwd(), train=False, download=False, transform=self.transform_test)
-        custom_cifar10_train = RandomMaskDataset(cifar10_train, 10)
+        custom_cifar10_train = RandomMaskDataset(cifar10_train, self.mask_pixels)
 
         self.train_dataset, self.val_dataset = random_split(custom_cifar10_train, [40000, 10000])
         self.test_dataset = cifar10_test
--- new_lightning_network.py
+++ new_lightning_network.py
@@ -2,6 +2,32 @@
 ## Integrate augmentations before input to sampler
 ## Save hyperparameters
 ## Profiler -- Done
+import os, argparse, random
+import torch
+import matplotlib.pyplot as plt
+from torch import nn
+from torch.optim import lr_scheduler
+import torchvision
+from torchvision import models
+from torch.utils.data import DataLoader
+from pytorch_lightning import LightningModule, Trainer
+import torch.nn.functional as F
+from dataloader import CustomCIFAR10DataModule
+
+import wandb
+
+labels_map = {
+    0: "airplane",
+    1: "automobile",
+    2: "bird",
+    3: "cat",
+    4: "deer",
+    5: "dog",
+    6: "frog",
+    7: "horse",
+    8: "ship",
+    9: "truck",
+}
 class FilterOutMask(nn.Module):
     """Implemeted using conv layers"""
     def __init__(self, k):
@@ -221,4 +247,46 @@ class Sampler_Classifer_Network(LightningModule):
 
     def test_step(self, batch, batch_idx):
         return None
+  
+if __name__ == '__main__':
+
+  parser = argparse.ArgumentParser(description='Hyperparam initializer')
+  parser.add_argument('-m', type=float, dest='mask_ratio', default=1.0,
+                        help = 'mask ratio')
+  parser.add_argument('-lp', dest='loop_param', type=int, default=5,
+                        help = 'loop parameter')
+  parser.add_argument('-context', dest='context', type=int, default=0,)
+                        
+
+  parser.add_argument('-save_folder', dest='save_path', type=str, default='',
+                        help = 'path to folder where to save objects')
+  parser.add_argument('-model_name', dest='model_name', type=str, default='baseline',
+                        help = 'name of model')
+  parser.add_argument('-pretrained_classifier', dest='pre_clr', type=int, default=0,
+                        help = 'pretrained_classifier')
+  parser.add_argument('-epochs', dest='epochs', type=int, default=20,
+                    help = 'epochs')
+  args = parser.parse_args()
+
+  save_path = "./vanilla_test"
+
+  if not os.path.exists("./vanilla_test"):
+    os.mkdir("./vanilla_test")
+
+
+  batch_size       = 64
+  epochs           = args.epochs
+  mask_per         = args.mask_ratio
+  loop_parameter   = args.loop_param
+  classifier_start = 0.25
+
+  CIFAR10_dm = CustomCIFAR10DataModule(int(mask_per*1024))
+  CIFAR10_dm.prepare_data()
+  CIFAR10_dm.setup()
+
+  sampler_model    = SamplerNetwork(int(mask_per*1024))
+  classifier_model = ClassifierNetwork()
+  main_model       = Sampler_Classifer_Network(sampler_model, classifier_model, loop_parameter, classifier_start, mask_per, save_path)
+  trainer          = Trainer(gpus=1, accelerator="gpu", max_epochs=40, profiler='simple')
+  trainer.fit(main_model, CIFAR10_dm)
 
